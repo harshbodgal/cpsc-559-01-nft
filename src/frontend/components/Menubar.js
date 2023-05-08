21442ae858f03f12fa8f9fdf924b6412ca244c3c
Apurva Gawande <100802168+ApurvaG04@users.noreply.github.com>
2023-05-07 18:40:24 -0700

    Update Menubar.js

--- src/frontend/components/Menubar.js
+++ src/frontend/components/Menubar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Navbar, Nav, Button, Container } from 'react-bootstrap'
 import Sports from './Sports.png'
 import Home from './Home.png'
@@ -15,22 +15,30 @@ const Menu = ({ web3Handler, wallet}) => {
                         <img src={Sports} width="45" height="45" className="" alt="" />
                         &nbsp; Sports NFT Marketplace
                     </Navbar.Brand>
-                    <Button to="/" activeClassName="active">
-                        <img src={Home} width="35" height="35" className="logo" alt="" />
+                    <Button>
+                        <Link to="/" activeClassName="active">
+                            <img src={Home} width="35" height="35" className="logo" alt="" />
+                        </Link>
                     </Button>
-                    <Button to="/Sell" activeClassName="active">
-                        Sell
-                    </Button>
-                    <Button to="/My-items" activeClassName="active">
-                        My Items
-                    </Button>
-                    <Button to="/Purchases" activeClassName="active">
-                        Purchases
-                    </Button>
+                    <Link to="/Sell" activeClassName="active">
+                        <Button>    
+                            Sell                       
+                        </Button>
+                    </Link>
+                    <Link to="/My-items" activeClassName="active">
+                        <Button>    
+                            My-items                       
+                        </Button>
+                    </Link>
+                    <Link to="/Purchases" activeClassName="active">
+                        <Button>    
+                            Purchases                      
+                        </Button>
+                    </Link>
                 </nav>
                 <Nav>
                     {wallet ? (
-                        <NavLink
+                        <Nav.Link
                             href={`https://etherscan.io/address/${wallet}`}
                             target="_blank"
                             rel="noopener noreferrer"
@@ -38,8 +46,7 @@ const Menu = ({ web3Handler, wallet}) => {
                             <Button variant="outline">
                                 {wallet.slice(0, 6) + '...' + wallet.slice(39, 42)}
                             </Button>
-
-                        </NavLink>
+                        </Nav.Link>
                     ) : (
                         <Button onClick={web3Handler} style={{backgroundColor: 'red' }}>Connect Metamask</Button>
                     )}
